9e1cd4b2396026f9705676c22f00701ab3b0ed0d
Unknown <froastj@gmail.com>
2019-03-03 12:09:39 -0800

    Another index

--- database/mongo.js
+++ database/mongo.js
@@ -1,2 +1,4 @@
 // Create text search indices on name and description of RSOs
 db.rsos.createIndex( { name: "text", description: "text" } );
+// Needed for regex
+db.rsos.createIndex( { name: 1 } );
